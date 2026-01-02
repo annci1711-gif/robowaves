f5702a7a19276224b181a2fd507e250262ccc5cb
annci <annci@Anncita>
2026-01-02 16:43:02 +0530

    first commit

---
+++ src/test/java/githubPractice/GitHubPracticeTest.java
@@ -0,0 +1,14 @@
+package githubPractice;
+
+import org.testng.annotations.Test;
+
+public class GitHubPracticeTest {
+	@Test
+	public void gitHubPracticeTest() {
+		System.out.println("hii github");
+		String data = System.getProperty("smart");
+	 System.out.println(data);
+		
+	}
+
+}
---
+++ src/test/java/jenkiensPractice/JenkiensPracticeTest.java
@@ -0,0 +1,11 @@
+package jenkiensPractice;
+
+import org.testng.annotations.Test;
+
+public class JenkiensPracticeTest {
+	@Test
+	public void jenkiensPracticeTest() {
+		System.out.println("hii jenkiens");
+	}
+
+}
